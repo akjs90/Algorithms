bb48e28a6bcd6a77bb21e0d36466841b56f02050
Lenovo <Lenovo@Lenovo-PC>
2016-10-18 23:00:11 +0530

    101hack41 algorithm washing plates added

---
+++ src/hack10141/washingplates/WashingPlates.java
@@ -0,0 +1,20 @@
+package hack10141.washingplates;
+
+import java.util.Scanner;
+
+public class WashingPlates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int p[]=new int[n];
+        int d[]=new int[n];
+        for(int i=0;i<n;i++){
+            p[i]=sc.nextInt();
+            d[i]=sc.nextInt();
+        }
+	}
+
+}
